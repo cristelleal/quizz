8fa8cb98d2c4767622f8fbff5af85e9a13fc74ed
cristelleal <cristelleal@gmail.com>
2024-02-16 18:14:38 +0100

    style: fix space between wave and footer components

--- src/components/wave/Wave.jsx
+++ src/components/wave/Wave.jsx
@@ -4,7 +4,7 @@ function Wave() {
       <svg
         xmlns='http://www.w3.org/2000/svg'
         viewBox='0 0 1440 320'
-        className='mt-auto bottom-0 lg:bottom-6'
+        className='inset-px mt-auto bottom-0 lg:bottom-6'
       >
         <path
           fill='#FEF2F2'
